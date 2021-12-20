a142fe9119a2d910af499fdc4b158efa3cb8e17e
YH_MealAM <s103033617@m103.nthu.edu.tw>
2021-12-20 18:36:44 +0800

    use merge to fasten the load data process

--- load_data.py
+++ load_data.py
@@ -22,42 +22,18 @@ with open('dm2021-lab2-hw2/tweets_DM.json') as jsonfile:
 
 tweets_df['id'] = tweets_id
 tweets_df['text'] = tweets_text
-tweets_df['identification'] = np.nan
 
-rows_ident = []
-rows_emotion = []
+tweets_ident = pd.read_csv('dm2021-lab2-hw2/data_identification.csv')
+tweets_ident = tweets_ident.rename(columns={"tweet_id": "id"})
 
-with open('dm2021-lab2-hw2/data_identification.csv') as csv_file:
-    next(csv_file)
-    for row in csv.reader(csv_file):
-        rows_ident.append(row)
-        pass
-    pass
+tweets_df = pd.merge(tweets_df, tweets_ident, on=['id'])
 
-with open('dm2021-lab2-hw2/emotion.csv') as emotion_file:
-    next(emotion_file)
-    for row in csv.reader(emotion_file):
-        rows_emotion.append(row)
-        pass
-    pass
+tweets_emotion = pd.read_csv('dm2021-lab2-hw2/emotion.csv')
+tweets_emotion = tweets_emotion.rename(columns={"tweet_id": "id"})
 
-tweets_df = tweets_df.sort_values("id")
-rows_ident.sort(key=lambda s: s[0])
-rows_ident_arr = np.array(rows_ident)
-tweets_df['identification'] = rows_ident_arr[:, 1]
-
-id_list = tweets_df.id.to_list()
-rows_emotion_arr = np.array(rows_emotion)
-id_emotion = rows_emotion_arr[:, 0].tolist()
-
-id_difference = list(set(id_list) - set(id_emotion))
-
-tweets_df_reindex = tweets_df.set_index("id")
-tweets_df_reindex = tweets_df_reindex.drop(id_difference)
-
-rows_emotion.sort(key=lambda s: s[0])
-tweets_df_reindex['emotion'] = rows_emotion_arr[:, 1]
+tweets_df = pd.merge(tweets_df, tweets_emotion, on=['id'])
 
 
-tweets_df_reindex.to_pickle('DS.pkl')
-print(len(tweets_df_reindex))
+tweets_df.to_pickle('DS_train.pkl')
+tweets_df.to_csv('DS_train.csv')
+print(len(tweets_df))
